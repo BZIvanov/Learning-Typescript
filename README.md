fa2f282ab56c880caae6a824afa1347c4ceb6705
BZIvanov <biser.z.ivanov@gmail.com>
2020-10-05 21:26:32 +0300

    removed demos files

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# BZIvanov.github.io
-web-site
+# README
--- SSITA/HangmanWithAngular/src/app/shared/interfaces/secret.ts
+++
@@ -1,7 +0,0 @@
-import ILetter from './anLetter';
-
-export default interface ISecret {
-    letters: Array<ILetter>;
-    hint: string;
-    word: string
-}
