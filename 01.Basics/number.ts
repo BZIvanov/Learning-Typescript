19db7c9446cfeb39f44202acba2a969d31e52446
BZIvanov <biser.z.ivanov@gmail.com>
2021-05-01 20:49:27 +0300

    void example

--- 01.Basics/number.ts
+++ 01.Basics/number.ts
@@ -1,5 +1,8 @@
-function sum(a: number, b: number) {
+// our function will return result of type number
+function sum(a: number, b: number): number {
   return a + b;
 }
 
-console.log(sum(3, 5));
+// here we don't need to specify, that the result variable is of type number because ts knows that from the return type of the function
+const result = sum(3, 5);
+console.log(result);
---
+++ 01.Basics/void.ts
@@ -0,0 +1,6 @@
+// return type of void is used, when we are not returning value
+function printName(name: string): void {
+  console.log(name);
+}
+
+printName('Iva');
