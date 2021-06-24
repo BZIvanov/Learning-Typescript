529bd99d61bbf6032c8bfa5b881324f6df1ffb00
BZIvanov <biser.z.ivanov@gmail.com>
2021-06-24 19:17:52 +0300

    type second example

--- 01.Basics/type.ts
+++ 01.Basics/type.ts
@@ -1,3 +1,5 @@
+// EXAMPLE 1
+
 // this way we can define type, which we can easily reuse, instead of typing the entire type object everywhere
 type User = { name: string; city: string; income: number };
 
@@ -11,3 +13,14 @@ const addUser = (user: User): void => {
 
 addUser(myUser);
 console.log(usersList);
+
+// EXAMPLE 2
+
+// basically type keyword is the same as type string for example, but used for more complicated types than just number or string
+type funcInput = string | number;
+
+function printIt(value: funcInput) {
+  return value;
+}
+
+console.log(printIt('Hello'));
