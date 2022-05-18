c4d4de50080e1ddfb4e792a9d1804ac51e6fea4c
BZIvanov <biser.z.ivanov@gmail.com>
2022-05-18 23:00:57 +0300

    react polymorphic component demo

---
+++ React/PolymorphicComponents/TextComponentDemo/src/App.tsx
@@ -0,0 +1,24 @@
+import CustomText from './components/CustomText';
+import EmphasizedText from './components/EmphasizedText';
+
+const App = () => {
+  return (
+    <div>
+      <CustomText>Default Props</CustomText>
+
+      <CustomText as='h2'>Valid html tag</CustomText>
+
+      <CustomText as='a' href='https://google.com' target='_blank'>
+        Valid combination between html tag and attributes for the tag
+      </CustomText>
+
+      <div>
+        <CustomText as={EmphasizedText}>
+          Component is also valid for React.ElementType
+        </CustomText>
+      </div>
+    </div>
+  );
+};
+
+export default App;
