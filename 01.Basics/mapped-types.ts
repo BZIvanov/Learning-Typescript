e022ce266dd5b4e048dcee43592e8d9868fce2a2
BZIvanov <biser.z.ivanov@gmail.com>
2023-03-11 16:08:59 +0200

    mapped types demo

---
+++ 01.Basics/mapped-types.ts
@@ -0,0 +1,15 @@
+type Movie = {
+  title: string;
+  year: number;
+};
+
+type Series = { [movieKey in 'Cool' | 'Nice']: Movie };
+
+function getMovies(movies: Series) {
+  return movies;
+}
+
+const movies = getMovies({
+  Cool: { title: 'Cool movie', year: 2023 },
+  Nice: { title: 'Nice movie', year: 2022 },
+});
--- 01.Basics/partial.ts
+++ 01.Basics/partial.ts
@@ -9,3 +9,7 @@ const myProduct: Partial<Product> = { name: 'Laptop' };
 
 // the below line will result in error, because with Partial making all the keys optional, the name key could be undefined, not always a string
 // const productName: string = myProduct.name;
+
+/*
+For more info on the Partial utility and other utility types check the docs here: https://www.typescriptlang.org/docs/handbook/utility-types.html
+*/
