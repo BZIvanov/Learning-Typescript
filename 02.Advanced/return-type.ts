9409dcbbfa786ee771a07e16437647360c9d0953
BZIvanov <ivanov.biser@gmx.com>
2024-01-29 20:21:33 +0200

    return type examples

---
+++ 02.Advanced/return-type.ts
@@ -0,0 +1,21 @@
+// ReturnType is a utility type that extracts the return type from a function type.
+
+// Example 1
+function getResult(flag: boolean): string | number {
+  return flag ? 'Success' : 404;
+}
+
+type ResultReturnType = ReturnType<typeof getResult>;
+
+// Example 2
+function fetchData(callback: (data: string) => void): Promise<string> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const data = 'Fetched data';
+      resolve(data);
+      callback(data);
+    }, 1000);
+  });
+}
+
+type FetchDataReturnType = ReturnType<typeof fetchData>;
